0f07fd893be490bd2d7c14376fa7d65ef23bbe1b
rumgalxdead <rumgal@gmail.com>
2022-06-21 16:30:23 +0200

    axion start

---
+++ src/components/Countries.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+const Countries = () => {
+	// le useEffect se jouen tlorsque le composant est monté
+	useEffect(() => {}, []);
+
+	return (
+		<div className="countries">
+			<h1>COUNTRIES</h1>
+		</div>
+	);
+};
+
+export default Countries;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Countries from "../components/Countries";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
@@ -8,26 +9,7 @@ const Home = () => {
 			<div className="container ">
 				<Logo />
 				<Navigation />
-				<h1 className="row">Accueil</h1>
-				<br />
-
-				<div className="row d-flex">
-					<div className="col">
-						<div className="container">
-							<h1>Hello React</h1>
-							<h2>Debut du cours</h2>
-						</div>
-					</div>
-					<div className="col">
-						<p className="col">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, ab molestiae nisi
-							officia aliquid fuga dicta laboriosam voluptatem eos aliquam commodi facere autem veritatis,
-							perspiciatis voluptates. Sint commodi adipisci eos exercitationem voluptatibus placeat
-							repellat deleniti, hic cum odio dolor sit vel natus dolorum doloribus reprehenderit aliquam
-							unde illum et incidunt?
-						</p>
-					</div>
-				</div>
+				<Countries />
 			</div>
 		</div>
 	);
